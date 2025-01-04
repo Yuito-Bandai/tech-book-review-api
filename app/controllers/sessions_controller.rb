5bfff81f6efbc1f1981d9d74c2b179b420f47471
Yuito-Bandai <yuiton1107@icloud.com>
2025-01-05 05:33:35 +0900

    存在しないユーザーでのログインを防ぐ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,19 @@
 class SessionsController < ApplicationController
-
   require 'jwt'
 
   # ログイン
   def create
+    # ユーザーを検索
     user = User.find_by(email: params[:email])
 
+    # ユーザーが存在しない場合やパスワードが一致しない場合にエラーを返す
+    if user.nil?
+      render json: { status: 401, message: "Invalid email or password" }, status: :unauthorized
+      return
+    end
+
     # ユーザーが存在し、パスワードが一致する場合
-    if user&.authenticate(params[:password])
+    if user.authenticate(params[:password])
       # JWTトークンを生成
       token = encode_token(user_id: user.id)
       Rails.logger.info("トークンを生成しました: #{token}")
@@ -16,7 +22,7 @@ class SessionsController < ApplicationController
       render json: { status: :created, token: token, user: user }
     else
       # 認証エラー
-      render json: { status: 401, message: "Invalid credentials" }
+      render json: { status: 401, message: "Invalid email or password" }, status: :unauthorized
     end
   end
 
@@ -31,7 +37,6 @@ class SessionsController < ApplicationController
   # JWTトークンのエンコード
   def encode_token(payload)
     # 'your_secret_key' を環境変数に変更してセキュリティを強化
-    # JWTのペイロードに有効期限(exp)を設定することも重要
     payload[:exp] = 24.hours.from_now.to_i  # 24時間後にトークンが期限切れになる
     JWT.encode(payload, ENV['SECRET_KEY_BASE'], 'HS256')  # 環境変数から秘密鍵を取得
   end
