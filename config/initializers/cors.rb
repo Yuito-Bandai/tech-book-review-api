101dc502ca1c01e2e6eed50b974cac6b6ebb0ca4
Yuito-Bandai <yuiton1107@icloud.com>
2025-01-11 03:02:58 +0900

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,14 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    if Rails.env.development?
+      origins 'http://localhost:3000' # 開発環境のフロントエンドURL
+    end
+
+    if Rails.env.production?
+      origins 'https://tech-book-review-frontend-bandais-projects.vercel.app/' # 本番環境のフロントエンドURL
+    end
+
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
