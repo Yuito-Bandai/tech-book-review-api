093dc7e3fbaf74eda2cbdf70e581ec70bbbbb436
Yuito-Bandai <yuiton1107@icloud.com>
2025-01-29 18:40:30 +0900

    domainに注意

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins 'http://localhost:3000' # 開発環境のフロントエンドURL
     end
 
+    # 末尾にスラッシュないこと確認!!!!!!
     if Rails.env.production?
       origins 'https://tech-book-review-frontend.vercel.app' # 本番環境のフロントエンドURL
     end
